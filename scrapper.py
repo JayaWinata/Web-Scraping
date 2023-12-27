eaa91168e11d5ec630e6c2f4e9ce2d2f6a4993d7
Jaya Winata <jayawinata100@gmail.com>
2023-12-27 18:23:02 +0700

    Source Code modified

--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,28 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
 url = 'https://rb.gy/ab50wv'
 options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 driver.set_window_size(1366,768)
 driver.get(url)
 
-time.sleep(10)
-driver.quit()
+page = driver.find_element(By.CSS_SELECTOR,'[data-tab_type="STANDINGS"]')
+page.click()
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "[aria-label='Bagian Klasemen']"))
+    )
+    source_code = driver.find_element(By.TAG_NAME, 'table').get_attribute('innerHTML')
+finally:
+    driver.quit()
+
+soup = BeautifulSoup(source_code,'html.parser')
+
+print(soup.encode())
